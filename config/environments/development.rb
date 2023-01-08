463971e1447e7c3bd495e6c92d2b5141b67ea7b7
riffiy <117071448+riffiy@users.noreply.github.com>
2023-01-08 08:41:58 +0000

    delete credentials_yml_enc

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,5 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.hosts << "f30399b32bbc4601b36124121fdf3195.vfs.cloud9.ap-northeast-1.amazonaws.com"
 end
