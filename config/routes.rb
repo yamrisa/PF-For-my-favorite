80281a861a7406f0ed0e837f12b9ef741682e431
riffiy <117071448+riffiy@users.noreply.github.com>
2023-01-11 07:19:08 +0000

    top/aboutの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
 # 管理者用
 # URL /admin/sign_in ...
 devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
@@ -11,6 +12,11 @@ devise_for :users,skip: [:passwords], controllers: {
   registrations: "public/registrations",
   sessions: 'public/sessions'
 }
+
+scope module: :public do
+    root to: 'homes#top'
+    get 'about' => 'homes#about', as: 'about'
+end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
