8e49a9efe83005bb75315c50d0a02ef8f018dddf
riffiy <117071448+riffiy@users.noreply.github.com>
2023-01-12 03:05:31 +0000

    add list/edit機能

--- app/controllers/public/lists_controller.rb
+++ app/controllers/public/lists_controller.rb
@@ -21,9 +21,13 @@ class Public::ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id]) 
   end
 
   def update
+    @list = List.find(params[:id])
+    @list.update(list_params)
+    redirect_to list_path
   end
 
   def destroy
