bce5f80f6dadcb9d5592a9ae3f3419131eb560b8
riffiy <117071448+riffiy@users.noreply.github.com>
2023-01-14 10:43:38 +0000

    add Bootstrap/FontAwesome導入

--- Gemfile
+++ Gemfile
@@ -65,3 +65,4 @@ gem "net-pop"
 gem "net-imap"
 
 gem 'devise'
+gem 'jquery-rails'
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "bootstrap";
+import "../stylesheets/application";
+
+import '@fortawesome/fontawesome-free/js/all'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
