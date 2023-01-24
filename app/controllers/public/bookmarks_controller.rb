2c4f6ceb8ca76265d5f494d8bf487375b411534a
riffiy <riffiy.6135@gmail.com>
2023-01-24 12:06:55 +0000

    add layout4

--- app/controllers/public/bookmarks_controller.rb
+++ app/controllers/public/bookmarks_controller.rb
@@ -2,6 +2,7 @@ class Public::BookmarksController < ApplicationController
   def index
     @bookmarks = Output.where(user_id: current_user.id)
     @colections = Colection.where(user_id: current_user.id).pluck(:output_id)
-    @like_outputs = Output.find(@colections).page(params[:page])
+    outputs = Output.find(@colections)
+    @like_outputs = Kaminari.paginate_array(outputs).page(params[:page])
   end
 end
