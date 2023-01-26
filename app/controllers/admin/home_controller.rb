15e517a4378bacf26159dcc021a2f8f94f8337d9
riffiy <riffiy.6135@gmail.com>
2023-01-26 13:25:43 +0000

    fix admin/top

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,7 +1,7 @@
 class Admin::HomeController < ApplicationController
   
   def top
-    @outputs = Output.all.page(params[:page])
+    @outputs = Output.where(release: 'release').page(params[:page])
   end
   
 end
