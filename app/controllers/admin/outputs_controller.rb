906f016e28807620b91317d8910b25a3393b6e03
riffiy <riffiy.6135@gmail.com>
2023-03-04 06:20:32 +0000

    fix管理者ステータス

--- app/controllers/admin/outputs_controller.rb
+++ app/controllers/admin/outputs_controller.rb
@@ -1,6 +1,8 @@
 class Admin::OutputsController < ApplicationController
-    
+  before_action :authenticate_admin!
+  
   def destroy
+    @admin = current_admin
     @output = Output.find(params[:id])
     @output.destroy
     redirect_to admin_home_top_path
